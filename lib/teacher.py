f7f457788a4c76924af77b95aacb03da1cca966e
Donell <donell.wambua@student.moringaschool.com>
2023-08-27 08:45:33 +0300

    Completed assignment

--- lib/teacher.py
+++ lib/teacher.py
@@ -1,10 +1,16 @@
 #!/usr/bin/env python
 
+
+
 from user import User
 
-import random
-
 class Teacher(User):
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)
+        self.knowledge = []
 
     def teach(self):
-        pass
+        if self.knowledge:
+            return random.choice(self.knowledge)
+        else:
+            return "I have no knowledge to share."
